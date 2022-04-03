6d2110e917f4746fbf177d1b520cd83c7362e3dc
kic <mingzhixian123@gmail.com>
2022-04-03 15:43:12 +0800

    blog debug

--- js/main.js
+++ js/main.js
@@ -45,3 +45,23 @@ $.ajax({
 		console.log("error,can not find article!");
 	});
 });
+
+//自动生成目录
+function directory() {
+    $("#articleBody").find("h1,h2,h3,h4,h5,h6").each(function (i, item) {
+        var tag = $(item).get(0).localName;
+        $(item).attr("id", "p" + i);
+        $("#articleContent").append('<p><a class = "title-' + tag + ' anchor-link" onclick="GoTo(\'#p' + i + '\')" >' + $(this).text() + '</a></p>');
+    });
+    $(".title-h1").css("margin-left", 0);
+    $(".title-h2").css("margin-left", 10);
+    $(".title-h3").css("margin-left", 20);
+    $(".title-h4").css("margin-left", 30);
+    $(".title-h5").css("margin-left", 40);
+    $(".title-h6").css("margin-left", 50);
+}
+
+//点击目录滚动到对应位置
+function GoTo(link) {
+    $("html,body").animate({scrollTop: $(link).offset().top}, 400);
+}
