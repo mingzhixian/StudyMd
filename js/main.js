5226be34b65b919d3b0522815cec5d5ebecd4572
kic <mingzhixian123@gmail.com>
2022-04-03 17:24:17 +0800

    blog v2

--- js/main.js
+++ js/main.js
@@ -5,11 +5,15 @@ function getQueryVariable(variable) {
 	for (var i = 0; i < vars.length; i++) {
 		var pair = vars[i].split("=");
 		if (pair[0] == variable) {
-			return "/"+pair[1];
+			return "/" + pair[1];
 		}
 	}
 	//为空情况
 	return ("/README.md");
+}
+
+function getTitle(variable) {
+	return variable.substring(variable.lastIndexOf("/") + 1, variable.lastIndexOf(".md"));
 }
 
 //获取文章名
@@ -18,6 +22,7 @@ var artUrl = decodeURIComponent(getQueryVariable("art"));
 //获取文章markdown文件
 $.ajax({
 	url: SiteUrl() + artUrl,
+	//url: "https://mingzhixian.github.io/StudyMd" + artUrl,
 	type: "get",
 }).done(function (output) {
 	//解析文章并填充文章名以及文章
@@ -26,9 +31,10 @@ $.ajax({
 	if (artUrl == "/README.md") {
 		artUrl = SiteTitle
 	};
-	$("#articleTitle").html(artUrl);
+	$("#articleTitle").html(getTitle(artUrl));
 	$("#articleBody").html(converter.makeHtml(text));
 	directory();
+	SetImg();
 }).fail(function () {
 	$.ajax({
 		url: SiteUrl() + "/README.md",
@@ -40,9 +46,10 @@ $.ajax({
 		if (artUrl == "/README.md") {
 			artUrl = SiteTitle
 		};
-		$("#articleTitle").html(artUrl);
+		$("#articleTitle").html(getTitle(artUrl));
 		$("#articleBody").html(converter.makeHtml(text));
 		directory();
+		SetImg();
 	}).fail(function (xhr, status) {
 		console.log("error,can not find article!");
 	});
@@ -50,20 +57,33 @@ $.ajax({
 
 //自动生成目录
 function directory() {
-    $("#articleBody").find("h1,h2,h3,h4,h5,h6").each(function (i, item) {
-        var tag = $(item).get(0).localName;
-        $(item).attr("id", "p" + i);
-        $("#articleContent").append('<p><a class = "title-' + tag + ' anchor-link" onclick="GoTo(\'#p' + i + '\')" >' + $(this).text() + '</a></p>');
-    });
-    $(".title-h1").css("margin-left", 0);
-    $(".title-h2").css("margin-left", 10);
-    $(".title-h3").css("margin-left", 20);
-    $(".title-h4").css("margin-left", 30);
-    $(".title-h5").css("margin-left", 40);
-    $(".title-h6").css("margin-left", 50);
+	$("#articleBody").find("h1,h2,h3,h4,h5,h6").each(function (i, item) {
+		var tag = $(item).get(0).localName;
+		$(item).attr("id", "p" + i);
+		$("#articleContent").append('<p class = "contentTitle"><a class = "contentTitle-' + tag + '" onclick="GoTo(\'#p' + i + '\')" >' + $(this).text() + '</a></p>');
+	});
+	$(".contentTitle-h1").css("margin-left", 0);
+	$(".contentTitle-h2").css("margin-left", 30);
+	$(".contentTitle-h3").css("margin-left", 60);
+	$(".contentTitle-h4").css("margin-left", 90);
+	$(".contentTitle-h5").css("margin-left", 120);
+	$(".contentTitle-h6").css("margin-left", 150);
 }
 
 //点击目录滚动到对应位置
 function GoTo(link) {
-    $("html,body").animate({scrollTop: $(link).offset().top}, 400);
+	$("html,body").animate({
+		scrollTop: $(link).offset().top
+	}, 400);
+}
+
+//处理图片显示问题
+function SetImg() {
+	$("#articleBody").find("img").each(function (i, item) {
+		var url = $(item).attr("src");
+		if (url.substring(0, 2) == "./") {
+			url = SiteUrl() + artUrl.substring(0, artUrl.lastIndexOf("/")+1) + url.substring(2);
+			$(item).attr("src", url);
+		}
+	})
 }
