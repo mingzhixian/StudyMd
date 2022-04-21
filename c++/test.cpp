f0a37e820d3143bd9e257a9fb91dbeb2b709a656
kic <mingzhixian123@gmail.com>
2022-04-21 16:22:03 +0800

    更新

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
+namespace first_space
+{
+    void func1()
+    {
+        cout << "命名空间1中的函数1" << endl;
+    }
+    namespace second_space
+    {
+        void func1()
+    {
+        cout << "命名空间2中的函数1" << endl;
+    }
+        void func2()
+        {
+            func1();
+            cout << "命名空间2中的函数2" << endl;
+        }
+    }
+}
+using namespace first_space::second_space;
 int main()
 {
-    cout << "sss" << endl;
+    func2();
     return 0;
 }
