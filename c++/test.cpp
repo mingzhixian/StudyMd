e8dbcf31e560c5ae1bb0f2f9362517475bff58fe
kic <mingzhixian123@gmail.com>
2022-04-21 15:06:10 +0800

    更新

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-namespace std
-{
-    void func1()
-    {
-        cout << "此处是调用了std空间中的cout" << endl;
-    }
-}
 using namespace std;
+
 int main()
 {
-    func1();
+    cout << "sss" << endl;
     return 0;
 }
