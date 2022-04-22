2190b0f6e530c5b99b57ba3d2d2fbc77a305fba7
kic <mingzhixian123@gmail.com>
2022-04-22 18:19:54 +0800

    更新

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,51 +1,41 @@
 #include <iostream>
- 
+
 using namespace std;
- 
+
 class Box
 {
-   public:
-      static int objectCount;
-      // 构造函数定义
-      Box(double l=2.0, double b=2.0, double h=2.0)
-      {
-         cout <<"构造函数" << endl;
-         length = l;
-         breadth = b;
-         height = h;
-         // 每次创建对象时增加 1
-         objectCount++;
-      }
-	  //计算体积
-      double Volume()
-      {
-         return length * breadth * height;
-      }
-	  //获取已创建对象
-      static int getCount()
-      {
-         return objectCount;
-      }
-   private:
-      double length;     // 长度
-      double breadth;    // 宽度
-      double height;     // 高度
+public:
+	static int objectCount;
+	static const int i1 = 1;
+	// static const float i2=1.0;报错
+	static const float i2;
+	// 构造函数定义
+	Box(double l = 2.0, double b = 2.0, double h = 2.0)
+	{
+		cout << "构造函数" << endl;
+		length = l;
+		breadth = b;
+		height = h;
+		// 每次创建对象时增加 1
+		objectCount++;
+	}
+	//计算体积
+	double Volume()
+	{
+		return length * breadth * height;
+	}
+
+private:
+	double length;	// 长度
+	double breadth; // 宽度
+	double height;	// 高度
 };
- 
-// 初始化类 Box 的静态成员
+
+//定义并初始化类 Box 的静态成员
 int Box::objectCount = 0;
- 
+const float Box::i2 = 2.0;
+
 int main(void)
 {
-  
-   // 在创建对象之前输出对象的总数
-   cout << "对象数：" << Box::getCount() << endl;
- 
-   Box Box1(3.3, 1.2, 1.5);    // 声明 box1
-   Box Box2(8.5, 6.0, 2.0);    // 声明 box2
- 
-   // 在创建对象之后输出对象的总数
-   cout << "对象数：" << Box::getCount() << endl;
- 
-   return 0;
+	return 0;
 }
